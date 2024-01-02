f6543cafe81ed6876f95318c5682bcfcd05bc9c0
ikozlenko <ikozlenko@toogoodtogo.com>
2024-01-02 12:55:43 +0100

    2015 day 1

--- src/main/kotlin/tools/FileUtils.kt
+++ src/main/kotlin/tools/FileUtils.kt
@@ -2,3 +2,6 @@ package tools
 
 fun readFile(filePath: String): List<String> =
      ClassLoader.getSystemResourceAsStream(filePath)!!.bufferedReader().readLines()
+
+fun readFileText(filePath: String): String =
+     ClassLoader.getSystemResourceAsStream(filePath)!!.bufferedReader().readText()
---
+++ src/main/kotlin/year2015/Day1.kt
@@ -0,0 +1,35 @@
+package year2015
+
+import tools.readFileText
+
+fun main() {
+    Day1().apply {
+        println("-=-=-=- part 1 -=-=-=-")
+        part1()
+        println("-=-=-=- part 2 -=-=-=-")
+        part2()
+    }
+}
+
+class Day1 {
+
+    private val input = readFileText("year2015/day1input")
+    fun part1() {
+        println(
+            input.map { if (it == '(') 1 else -1 }.sum()
+        )
+    }
+
+    fun part2() {
+        run breaker@{
+            input.map { if (it == '(') 1 else -1 }
+                .foldIndexed(0) { index, sum, i ->
+                    if (sum < 0) {
+                        println(index)
+                        return@breaker sum
+                    }
+                    sum + i
+                }
+        }
+    }
+}
