0593b99f02bac26de2ecfa7c5fd87096218ad08f
ikozlenko <ikozlenko@toogoodtogo.com>
2024-01-02 13:36:06 +0100

    2015 day 4

---
+++ src/main/kotlin/year2015/Day4.kt
@@ -0,0 +1,50 @@
+package year2015
+
+
+import java.security.MessageDigest
+
+fun main() {
+    Day4().apply {
+        println("-=-=-=- part 1 -=-=-=-")
+        part1()
+        println("-=-=-=- part 2 -=-=-=-")
+        part2()
+    }
+}
+
+class Day4 {
+
+    private val input = "yzbqklnj"
+
+    fun part1() {
+        var i = 0
+        while (true) {
+            val md5 = md5(input + i)
+            if (md5.startsWith("00000")) {
+                println(i)
+                break
+            }
+            i++
+        }
+    }
+
+    fun part2() {
+        var i = 0
+        while (true) {
+            val md5 = md5(input + i)
+            if (md5.startsWith("000000")) {
+                println(i)
+                break
+            }
+            i++
+        }
+    }
+
+    private fun md5(input: String): String {
+        val md = MessageDigest.getInstance("MD5")
+        val digested = md.digest(input.toByteArray())
+        return digested.joinToString("") {
+            String.format("%02x", it)
+        }
+    }
+}
