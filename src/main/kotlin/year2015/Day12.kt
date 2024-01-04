dd0a5fc6bcc4776f79d57f8007f92042c1e51fb7
ikozlenko <ikozlenko@toogoodtogo.com>
2024-01-04 15:14:04 +0100

    2015 day 12 part 1

--- src/main/kotlin/year2015/Day12.kt
+++ src/main/kotlin/year2015/Day12.kt
@@ -15,8 +15,33 @@ class Day12 {
 
     private val input = readFileText("year2015/day12input")
 
-    fun part1() {
+    fun part1map() {
+        val digits = input.map { if (it == '-') it else it.digitToIntOrNull() }
+        var sum = 0
+        var number = ""
+        var plus = true
+        digits.forEach {
+            when (it) {
+                '-' -> plus = false
+                null -> {
+                    if (number.isNotEmpty()) {
+                        if (plus) sum += number.toInt() else sum -= number.toInt()
+                        number = ""
+                        plus = true
+                    }
+                }
 
+                else -> number += it
+            }
+        }
+
+        println(sum)
+    }
+
+    fun part1() {
+        val regex = Regex("-?\\d+")
+        val numbers = regex.findAll(input).map { it.value.toInt() }.toList()
+        println(numbers.sum())
     }
 
     fun part2() {
